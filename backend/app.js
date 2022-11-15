a869fd7f77b42a4698ec76670035705e0066c35b
Dilhan Ceran <dilhan.ceran98@gmail.com>
2022-11-15 20:25:19 +0100

    15/11

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,17 @@ mongoose.connect('mongodb+srv://DilhanC:YBLD5yV7rKOfRcXp@cluster0.e9v1s16.mongod
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+ 
+app.use((req, res) => {
+  res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
 
 module.exports = app 
